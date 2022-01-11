3c0054654c240f6b78798ae7ce4f6d96d9310c0d
Curtis Brooks <curtisbrooks678@gmail.com>
2022-01-10 16:15:34 -0800

    WIP AccountController
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>

---
+++ RecipeBox/Models/Recipe.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace RecipeBox.Models
+{
+  public class Recipe
+  {
+    public Recipe()
+    {
+      this.JoinEntities = new HashSet<CategoryRecipe>(); 
+    }
+    public int RecipeId { get; set; }
+    public string Name { get; set; }
+    public string Instructions { get; set; }
+    public string Ingredients { get; set; }
+    public int[] Rating = new[] { 1, 2, 3, 4, 5 };
+    public virtual ApplicationUser User { get; set; }
+    public virtual ICollection<CategoryRecipe> JoinEntities { get; }
+  }
+}
